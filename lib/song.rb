999ead20d8339bc9aa0951c171d9162d3cd2443c
Sam Ferebee <sam.ferebee@gmail.com>
2021-01-14 15:33:08 -0500

    commit tree a600a9ff7cc69749d6c93e926ca79871f44a401a

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class Song
+  
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -10,4 +13,20 @@ class Song
     self.class.all << self
   end
 
+  def self.create()
+    new_song = Song.new
+    self.all.push(new_song)
+    new_song
+  end
+
+  def self.new_by_name(title)
+    new_song = Song.new
+    new_song.name =(title)
+    self.all.push(new_song)
+  end
+
+
+
 end
+
+binding.pry
